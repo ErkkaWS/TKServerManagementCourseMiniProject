19fa65e7b496b572c52ec18eebbdd2ddb09c971c
EricWSetälä <bhq708@myy.haaga-helia.fi>
2026-04-29 23:19:26 +0300

    Change Discord webhook URL in yhteydenotto.php
    Updated Discord webhook URL in yhteydenotto.php.

---
+++ ADCP_project/roles/php/files/yhteydenotto.php
@@ -0,0 +1,23 @@
+<?php
+$webhook_url = "https://discord.com/api/webhooks/1499141747085934654/U5DNDebeo_1Hiwo4-nfComUPUhZHupK8lW75gVVGeuupLYCuAu2y0lkmDEPtNAx99xRm";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nimi = htmlspecialchars($_POST["nimi"]);
+    $viesti = htmlspecialchars($_POST["viesti"]);
+    
+    $data = json_encode([
+        "content" => "**Yhteydenotto:**\nNimi: $nimi\nViesti: $viesti"
+    ]);
+    
+    $ch = curl_init($webhook_url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    curl_close($ch);
+    
+    echo "<script>window.location='/';</script>";
+    exit;
+}
+?>
